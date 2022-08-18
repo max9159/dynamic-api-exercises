54164e571fd2c21e397ed66f83f2058a56a158ce
Max Chou <max9159@gmail.com>
2022-08-18 23:49:53 +0800

    Add express adjust api path to /api/salaries

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-const { ApolloServer } = require('apollo-server')
-const { typeDefs } = require('./data/schema')
-const { resolvers } = require('./data/resolvers')
+var http = require('http');
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageLocalDefault
+} = require('apollo-server-core');
+const { typeDefs } = require('./data/schema');
+const { resolvers } = require('./data/resolvers');
 
-const port = process.env.PORT || 9090;
+async function startApolloServer() {
 
-const server = new ApolloServer({ resolvers, typeDefs });
+  const app = express();
+  const httpServer = http.createServer(app);
+  const port = process.env.PORT || 9090;
 
-server.listen({ port }, () => console.log(`Server Running: http://localhost:${port}`));
+  const server = new ApolloServer({
+    resolvers,
+    typeDefs,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+  await server.start();
+
+  // Additional middleware can be mounted at this point to run before Apollo.
+  // app.use('*', jwtCheck, requireAuth, checkScope);
+
+  // Mount Apollo middleware here.
+  server.applyMiddleware({ app, path: '/api/salaries' });
+  await new Promise(resolve => httpServer.listen({ port }, resolve));
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`);
+}
+startApolloServer();
